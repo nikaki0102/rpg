2b82d5b8c58fed20e02982de36095d4868e343a7
nikiaki0102 <nicholaslimabr@gmail.com>
2024-08-22 21:04:31 -0300

    finais

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const avanca = document.querySelectorAll('.btn-proximo');
 
 avanca.forEach(button => {
-    button.addEventListener('click', function(){
+    button.addEventListener('click', function () {
         const atual = document.querySelector('.ativo');
         const proximoPasso = 'passo-' + this.getAttribute('data-proximo');
 
@@ -9,3 +9,16 @@ avanca.forEach(button => {
         document.getElementById(proximoPasso).classList.add('ativo');
     })
 });
+// const parag = document.querySelectorAll("p");
+// parag.forEach(item => {
+//     item.addEventListener('click', function () {
+//         var prox =  this.nextSibling.nextSibling;
+//         prox.classList.remove('hide');
+//         if (prox.classList.contains('fim')) {
+//             prox.classList.remove('fim');
+//             var final =  prox.nextElementSibling;
+//             final.style.display = 'block';
+//             final.classList.add('center');
+//         }
+//     })
+// });
